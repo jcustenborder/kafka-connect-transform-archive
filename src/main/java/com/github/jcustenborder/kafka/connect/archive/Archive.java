b9923bdbb7a0bf0389bca5c18fc5829fea1b427e
Iosif Nicolae <iosif@mailo.dev>
2022-09-06 14:15:29 +0300

    Extract timestamp year, month and day

--- src/main/java/com/github/jcustenborder/kafka/connect/archive/Archive.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/archive/Archive.java
@@ -24,6 +24,9 @@ import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.transforms.Transformation;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,14 +49,22 @@ public class Archive<R extends ConnectRecord<R>> implements Transformation<R> {
         .name("com.github.jcustenborder.kafka.connect.archive.Storage")
         .field("key", r.keySchema())
         .field("key_string", Schema.STRING_SCHEMA)
+        .field("timestamp_year", Schema.INT8_SCHEMA)
+        .field("timestamp_month", Schema.INT8_SCHEMA)
+        .field("timestamp_day", Schema.INT8_SCHEMA)
         .field("partition", Schema.INT64_SCHEMA)
         .field("value", r.valueSchema())
         .field("topic", Schema.STRING_SCHEMA)
         .field("headers", Schema.STRING_SCHEMA)
         .field("timestamp", Schema.INT64_SCHEMA);
+    Calendar recordDate = new GregorianCalendar();
+    recordDate.setTimeInMillis(r.timestamp());
     Struct value = new Struct(schema)
         .put("key", r.key())
         .put("key_string", String.valueOf(r.key()).replaceAll("[^\\x00-\\x7F]", ""))
+        .put("timestamp_year", recordDate.get(Calendar.YEAR))
+        .put("timestamp_month", recordDate.get(Calendar.MONTH))
+        .put("timestamp_day", recordDate.get(Calendar.DAY_OF_MONTH))
         .put("partition", r.kafkaPartition())
         .put("value", r.value())
         .put("topic", r.topic())
@@ -64,8 +75,14 @@ public class Archive<R extends ConnectRecord<R>> implements Transformation<R> {
   private R applySchemaless(R r) {
     final Map<String, Object> archiveValue = new HashMap<>();
 
+    Calendar recordDate = new GregorianCalendar();
+    recordDate.setTimeInMillis(r.timestamp());
+
     archiveValue.put("key", r.key());
     archiveValue.put("key_string", String.valueOf(r.key()).replaceAll("[^\\x00-\\x7F]", ""));
+    archiveValue.put("timestamp_year", recordDate.get(Calendar.YEAR));
+    archiveValue.put("timestamp_month", recordDate.get(Calendar.MONTH));
+    archiveValue.put("timestamp_day", recordDate.get(Calendar.DAY_OF_MONTH));
     archiveValue.put("value", r.value());
     archiveValue.put("topic", r.topic());
     archiveValue.put("partition", r.kafkaPartition());
